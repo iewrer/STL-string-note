9722b81899509e2f734fd6ff6f6bf9a2037f946e
Wankai Zhang <wankaizhang@gmail.com>
2015-04-30 01:40:43 +0800

    Update basic_string.h

--- basic_string.h
+++ basic_string.h
@@ -3,6 +3,11 @@ namespace __gnu_cxx {
 template<typename _CharT, typename _Traits, typename _Alloc>
 class basic_string
 {
+ public:
+  // 
+  typename _Alloc::template rebind<_CharT>::other _CharT_alloc_type;
+  
+  
 };
 
 }
